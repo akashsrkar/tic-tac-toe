304e88962cf2f85d2c4c12a35f8ae770c5dbf723
akashsrkar <akashsarkar.dev@gmail.com>
2024-06-22 19:36:16 +0530

    refactor: moved js to separate file

---
+++ index.js
@@ -0,0 +1,57 @@
+const boxes = document.querySelectorAll("#board div");
+const playerInputs = [];
+const player1 = "X";
+const player2 = "O";
+
+let turn = 0;
+let nextTurn = player1;
+
+for (let i = 0; i < boxes.length; i++) {
+  let box = boxes[i];
+  box.addEventListener("click", () => {
+    if (turn === 9) {
+      const ans = checkWinner(playerInputs);
+      console.log(ans);
+    }else{
+    turn += 1;
+    box.textContent = nextTurn;
+    playerInputs[i] = nextTurn;
+    nextTurn = nextTurn === player1 ? player2 : player1;
+    }
+
+  });
+}
+
+function checkWinner(arr) {
+  const winPatterns = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [2, 4, 6],
+    [0, 4, 8],
+  ];
+
+  let result = "Draw";
+
+  for (let i = 0; i < winPatterns.length; i++) {
+    let [a, b, c] = winPatterns[i];
+    let condition =
+      arr[a] === arr[b] && arr[b] === arr[c] && arr[c] === arr[a];
+
+    if (condition) {
+      result = arr[a];
+    }
+  }
+
+  return result === "Draw" ? "The match is a draw" : `Winner is ${result}`;
+}
+
+// 1. Show the inputs and whose turn in the screen.
+// 2. Check for winner in every click and do auto check if all 9 clicks are over.
+// 3. Do not allow click after 9 click.
+// 4. Show to winner on screen.
+// 5. Make the codebase modular.
+    
