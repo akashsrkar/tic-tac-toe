33d9c214993ba569b6fced2fc3254814178169ea
akashsrkar <akashsarkar.dev@gmail.com>
2024-06-24 06:23:32 +0530

    refactor: made the codebase modular

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+import checkWinner from "./utils/checkWinner.js";
+
 const boxes = document.querySelectorAll("#board div");
 const showTurn = document.querySelector("#show-turn strong");
 const displayWinner = document.querySelector("#display-winner");
@@ -25,31 +27,6 @@ for (let i = 0; i < boxes.length; i++) {
       displayWinner.textContent = `winner is ${ans}`;
     }
   });
-}
-
-function checkWinner(arr) {
-  const winPatterns = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [2, 4, 6],
-    [0, 4, 8],
-  ];
-
-  for (let i = 0; i < winPatterns.length; i++) {
-    let [a, b, c] = winPatterns[i];
-
-    let condition = arr[a] === arr[b] && arr[b] === arr[c] && arr[c] === arr[a];
-
-    if (condition && arr[a] !== undefined) {
-       return arr[a];
-    }
-  }
-
-  return false;
 }
 
 // 1. Show the inputs and whose turn in the screen.
---
+++ utils/checkWinner.js
@@ -0,0 +1,26 @@
+function checkWinner(arr) {
+    const winPatterns = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [2, 4, 6],
+      [0, 4, 8],
+    ];
+  
+    for (let i = 0; i < winPatterns.length; i++) {
+      let [a, b, c] = winPatterns[i];
+  
+      let condition = arr[a] === arr[b] && arr[b] === arr[c] && arr[c] === arr[a];
+  
+      if (condition && arr[a] !== undefined) {
+         return arr[a];
+      }
+    }
+  
+    return false;
+  }
+
+  export default checkWinner;
