dd4af7abdaedfc6ed34f693a3a931597d44ec3c2
akashsrkar <akashsarkar.dev@gmail.com>
2024-06-25 10:38:06 +0530

    refactor: separation of concerns

--- index.js
+++ index.js
@@ -10,31 +10,46 @@ const player2 = "O";
 
 let turn = 0;
 let currentTurn = player1;
-let isWinnerDeclared = false;
+let gameStatus = checkGameStatus();
 
 showTurn.textContent = currentTurn;
 
 for (let i = 0; i < boxes.length; i++) {
   let box = boxes[i];
   box.addEventListener("click", () => {
-    if (!isWinnerDeclared && turn < 9) {
-      turn += 1;
-      box.textContent = currentTurn;
-      playerInputs[i] = currentTurn;
-      currentTurn = currentTurn === player1 ? player2 : player1;
-      showTurn.textContent = currentTurn;
-
-      const ans = checkWinner(playerInputs);
-      if (ans) {
-        displayWinner.textContent = `winner is ${ans}`;
-        isWinnerDeclared = true;
-      }else if(turn === 9){
-        displayWinner.textContent = "The match is drawn"
-      }
+    if (gameStatus) {
+      continueGame(box, i);
     }
   });
 }
 
+function checkGameStatus() {
+  const winner = checkWinner(playerInputs);
+  const maxTurn = 9;
+  return !winner && turn < maxTurn;
+}
+
+function continueGame(box, index) {
+  turn += 1;
+  box.textContent = currentTurn;
+  playerInputs[index] = currentTurn;
+  currentTurn = currentTurn === player1 ? player2 : player1;
+  showTurn.textContent = currentTurn;
+  gameStatus = checkGameStatus();
+  if (!gameStatus) {
+    endGame();
+  }
+}
+
+function endGame() {
+  const winner = checkWinner(playerInputs);
+  showTurn.textContent = "Game Over";
+  if (!winner) {
+    displayWinner.textContent = "The match is a draw";
+    return;
+  }
+  displayWinner.textContent = `winner is ${winner}`;
+}
 
 // 1. Bring in the logic of win and draw together in a function and also change the display and remove the current turn and replace it with "Game over".
 
